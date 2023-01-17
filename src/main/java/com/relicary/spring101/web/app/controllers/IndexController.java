b3208bb600f6741c84cfa202199d9b64b2b9e8ff
relicary <relicary@gmail.com>
2023-01-17 19:50:30 +0100

    View Controller: Use Controller in View with Model class

--- src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
+++ src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
@@ -1,13 +1,17 @@
 package com.relicary.spring101.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("title", "Hi Spring Framework!");
+
         return "index";
     }
 }
