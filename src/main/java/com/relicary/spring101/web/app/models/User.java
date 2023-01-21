1aeb4004a35f3eb6e3942e97862096ce8d2f5e68
relicary <relicary@gmail.com>
2023-01-21 11:25:18 +0100

    Model: POJO User class added

---
+++ src/main/java/com/relicary/spring101/web/app/models/User.java
@@ -0,0 +1,12 @@
+package com.relicary.spring101.web.app.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String firstName;
+    private String lastName;
+}
