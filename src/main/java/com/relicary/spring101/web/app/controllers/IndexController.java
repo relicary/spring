16c9081b51fc6a8a4611c5f5d5372d09bfd312b0
relicary <relicary@gmail.com>
2023-01-21 11:13:17 +0100

    Request Mapping: Add a PATH for a Controller

--- src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
+++ src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.relicary.spring101.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"/index", "/", "/home"})
+    @GetMapping({"", "/index", "/", "/home"})
     public String index(Model model){
 
         model.addAttribute("title", "Hi Spring Framework with Model!");
