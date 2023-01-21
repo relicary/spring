9b298da09332bd1cdf61fac093f3b21e0df2cccc
relicary <relicary@gmail.com>
2023-01-21 10:57:00 +0100

    View Controller: Use Controller in View with Map class

--- src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
+++ src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
@@ -1,16 +1,17 @@
 package com.relicary.spring101.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
-    public String index(ModelMap model){
+    public String index(Map<String, Object> map){
 
-        model.addAttribute("title", "Hi Spring Framework!");
+        map.put("title", "Hi Spring Framework with Map!");
 
         return "index";
     }
