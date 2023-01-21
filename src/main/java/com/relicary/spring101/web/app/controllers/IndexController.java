0be35dd72db5696f881d63d05a6b5e5e0ab9f6d9
relicary <relicary@gmail.com>
2023-01-21 11:28:48 +0100

    Model: Get Profile controller added

--- src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
+++ src/main/java/com/relicary/spring101/web/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.relicary.spring101.web.app.controllers;
 
+import com.relicary.spring101.web.app.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,5 +16,16 @@ public class IndexController {
         model.addAttribute("title", "Hi Spring Framework with Model!");
 
         return "index";
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(Model model) {
+        User user = new User();
+        user.setFirstName("Ana");
+        user.setLastName("Almeida");
+
+        model.addAttribute("user", user);
+
+        return "profile";
     }
 }
